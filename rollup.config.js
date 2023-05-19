27392a7f177e03189846d78b3913433d2c1a0cf6
Jack Cranston <jcranston94@gmail.com>
2023-05-19 12:40:03 +0100

    Fixed issues with exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,26 @@
-import terser from '@rollup/plugin-terser';
+const outputDefaults = {
+  name: 'ChartjsPluginA11y',
+  indent: false,
+  sourcemap: true,
+};
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/chartjs-plugin-a11y.min.js',
-    name: 'ChartjsPluginA11y',
-    format: 'umd',
-  },
-  plugins: [terser()],
+  output: [
+    {
+      ...outputDefaults,
+      file: 'dist/chartjs-plugin-a11y.umd.js',
+      format: 'umd',
+    },
+    {
+      ...outputDefaults,
+      file: 'dist/chartjs-plugin-a11y.js',
+      format: 'esm',
+    },
+    {
+      ...outputDefaults,
+      file: 'dist/chartjs-plugin-a11y.cjs',
+      format: 'cjs',
+    },
+  ],
 };
